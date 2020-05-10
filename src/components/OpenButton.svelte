7afe5bb826d88e8b513b6bcb73872c99c4595661
Parker McMullin <parker.mcmullin01@gmail.com>
2020-05-09 21:50:39 -0700

    Update colors and styling of door, panel, display, and buttons. Looking 🔥

--- src/components/OpenButton.svelte
+++ src/components/OpenButton.svelte
@@ -7,5 +7,6 @@ const dispatch = createEventDispatcher();
     id="open-button"
     on:click={() => dispatch('press')}
     on:keyPress={() => dispatch('press')}
-    class="row-start-6 row-span-1 col-start-5 col-span-1 bg-orange-200">
+    class="row-start-6 row-span-1 col-start-5 col-span-1 bg-gray-900 border border-gray-800 rounded-md p-1">
+    <div class="h-full w-full border border-gray-700 rounded"></div>
 </button>

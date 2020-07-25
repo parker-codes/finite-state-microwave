46a178fc3a8f8d181f8e0879bf0634275577d947
jwlarocque <jwlarocque@gmail.com>
2020-07-25 11:59:43 -0600

    Limit add 30s to 99:99, display times > 99:59 as 99:seconds

--- src/helpers.js
+++ src/helpers.js
@@ -10,7 +10,13 @@ export function formatDigits(digits) {
 }
 
 export function formatTimer(timer) {
-  const minutes = pad(Math.floor(timer / 60));
-  const seconds = pad(timer % 60);
-  return `${minutes}:${seconds}`;
+  let minutes, seconds;
+  if (timer < 6000) { // <= 99:59
+    minutes = Math.floor(timer / 60);
+    seconds = timer % 60;
+  } else { // limit minutes to two digits
+    minutes = 99;
+    seconds = timer - minutes * 60;
+  }
+  return `${pad(minutes)}:${pad(seconds)}`;
 }
